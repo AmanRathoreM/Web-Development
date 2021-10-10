feff6d5387e0f1f87cbba79f013725461b3c87fe
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-10-10 17:18:18 +0530

    Add files via upload

---
+++ CodeWithHarry/JavaScript/tutorial_12.js
@@ -0,0 +1,19 @@
+//* Date 10-10-2021
+
+//* In this tutorial you will learn about Understanding DOM and how to create a Website layout
+
+
+console.log(document)
+console.log(document.all)
+
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms[0];
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// })
+a = document.links[0];
+// use document.images and document.scripts and print the list of images and scripts on an html page
+console.log(a);
+
---
+++ CodeWithHarry/JavaScript/tutorial_13.js
@@ -0,0 +1,13 @@
+//* Date 10-10-2021
+
+//* In this tutorial you have to solve an Exercise
+
+
+//? Exercise 1
+//? You have to create a variable which is a string containing the text which is a link you are interested in. 
+
+//? You have to fetch all the links from a given page which contains this text
+
+//? codewithharry.com 
+//? javascript
+
